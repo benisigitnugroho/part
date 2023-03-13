6471dded1ef6bdc3f3ed9987d162c28e45f47746
benisigitnugroho <127712763+benisigitnugroho@users.noreply.github.com>
2023-03-13 12:11:04 +0700

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+	// Mendeklarasikan variabel dan memberi nilai
+	int A = 10;
+	int B = 10;
+	
+	// Melakukan Increment
+	A++; //sama dengan A+1
+	
+	//Melalkukan Decrement
+	B--; //Sama dengan B-1
+	
+	//Menampilkan nilai A akhir
+	cout<<"Nilai A akhir : "<<A<<endl;
+	
+	//Menampilkan nilai B akhir
+	cout<<"Nilai B akhir : "<<B<<endl;
+}
